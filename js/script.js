221a2e69b483a4c23e6868d95a3a728e30507bbe
Wesam-Ahmed <wesamahmedelsayedkhaled@gmail.com>
2021-08-09 22:54:02 +0200

    tour and travel website

---
+++ js/script.js
@@ -0,0 +1,58 @@
+let searchBtn = document.querySelector('#search-btn');
+let searchBar = document.querySelector('.search-bar-container');
+let formBtn = document.querySelector('#login-btn');
+let loginForm = document.querySelector('.login-form-container');
+let formClose = document.querySelector('#form-close');
+let formregClose=document.querySelector('#formreg-close');
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+let videoBtn = document.querySelectorAll('.vid-btn');
+let registerForm=document.querySelector('.register-form-container');
+let register=document.querySelector('#reg');
+
+window.onscroll = () =>{
+    searchBtn.classList.remove('fa-times');
+    searchBar.classList.remove('active');
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+    loginForm.classList.remove('active');
+    registerForm.classList.remove('active');
+}
+
+menu.addEventListener('click', () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+});
+
+searchBtn.addEventListener('click', () =>{
+    searchBtn.classList.toggle('fa-times');
+    searchBar.classList.toggle('active');
+});
+
+formBtn.addEventListener('click', () =>{
+    loginForm.classList.add('active');
+});
+
+
+formClose.addEventListener('click', () =>{
+    loginForm.classList.remove('active');
+});
+register.addEventListener('click', () =>{
+  registerForm.classList.add('active');
+});
+
+formregClose.addEventListener('click', () =>{
+  registerForm.classList.remove('active');
+});
+
+
+
+videoBtn.forEach(btn =>{
+    btn.addEventListener('click', ()=>{
+        document.querySelector('.controls .active').classList.remove('active');
+        btn.classList.add('active');
+        let src = btn.getAttribute('data-src');
+        document.querySelector('#video-slider').src = src;
+    });
+});
+
